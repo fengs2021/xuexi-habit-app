eb5b51e9d0fc987f2bb293e156a89fc4de10a273
fengs <fengs@github.com>
2026-03-25 10:51:40 +0800

    chore: 清理重复的兑换审批API
    1. 删除前端未使用的 api/exchange.js
    2. 更新后端 exchanges.js 路由注释，说明审批已迁移到 /approvals/*
    3. 统一审批入口使用 /api/approvals/*

--- frontend/src/api/exchange.js
+++
@@ -1,9 +0,0 @@
-import request from './request'
-
-export function getPendingExchanges() {
-  return request({ url: '/exchanges/pending', method: 'GET' })
-}
-
-export function approveExchange(id, data) {
-  return request({ url: '/exchanges/' + id + '/approve', method: 'PUT', data })
-}
